0d1a065f246a2b486c5919b2df758fda8a05fed8
iamsajjad <iamsajjad@mail.ru>
2020-04-16 11:20:09 +0300

    change title and function name

--- server/static/map/js/map.js
+++ server/static/map/js/map.js
@@ -1,17 +1,15 @@
 
-function PlanckMap(x, y, z) {
+function compromiserMap(x, y, z) {
 
   let date = document.getElementById('date').innerHTML;
-  var map = L.map('PlanckMap').setView([x, y], 8);
+  var map = L.map('compromiserMap').setView([x, y], 8);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-
-
-
   L.marker([x, y]).addTo(map)
       .bindPopup('Last Signal From The Object <br> ' + date)
       .openPopup();
 }
+
